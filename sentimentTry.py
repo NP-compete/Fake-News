12982f54b0adbb8ff2904d5649634ae53c5db07b
Ankit Kumar <48655921+Ankit687@users.noreply.github.com>
2019-10-29 14:58:34 +0530

    added space

--- sentimentTry.py
+++ sentimentTry.py
@@ -2,6 +2,7 @@ import tflearn
 from tflearn.data_utils import to_categorical, pad_sequences
 from tflearn.datasets import imdb
 
+
 #IMDB Dataset loading
 
 train, test, _ = imdb.load_data(path='imdb.pkl', n_words=10000, valid_portion=0.1)
